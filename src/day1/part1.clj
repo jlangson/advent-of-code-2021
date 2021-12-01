4d15d6ba191ac59ac4fa616204d92128d0564c13
Jared Langson <langson.jared@gmail.com>
2021-12-01 13:44:35 -0500

    Solve day1 part2

--- src/day1/part1.clj
+++ src/day1/part1.clj
@@ -25,10 +25,22 @@
        (str/split-lines)
        (map #(Integer/parseInt %))))
 
-(defn sliding-window)
+(defn sliding-window [num-vector]
+  (->> num-vector
+       (partition 3 1 [0])
+       (map #(apply + %))
+       (count-depth-increases)))
 
 
 (comment
   (count-depth-increases test-input)
   (count-depth-increases (read-numeric-file "src/day1/input.txt"))
+  (sliding-window test-input)
+  (map #(apply + %) '((1 2 3) (2 3 4) (3 4 5) (4 5 6)))
+  ;; => (6 9 12 15)
+
+  (map #(list (apply + %)) '((1 2 3) (2 3 4) (3 4 5) (4 5 6)))
+  ;; => ((6) (9) (12) (15))
+
+  (sliding-window (read-numeric-file "src/day1/input.txt"))
   )
