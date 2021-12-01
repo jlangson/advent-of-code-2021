86fa698b71ae10b698edac3f306f7743043a1294
Jared Langson <langson.jared@gmail.com>
2021-12-01 08:47:47 -0500

    Solve day1 part1

--- src/day1/part1.clj
+++ src/day1/part1.clj
@@ -1,24 +1,34 @@
-(ns day1.part1)
+(ns day1.part1
+  (:require [clojure.string :as str]))
 
-(def small-input
+(def test-input
   [199
    200
    208
    210
-   200
+   200                                                      ;; drop
    207
    240
    269
-   260
+   260                                                      ;; drop
    263])
 
 (defn count-depth-increases [input]
-  (filter (fn [list]
-            (> (second list) (first list))) input)
-  )
+  (->> input
+       (partition 2 1 [0])
+       (filter (fn [[a b]] (< a b)))
+       (count)))
+
+(defn read-numeric-file [path]
+  (->> path
+       (slurp)
+       (str/split-lines)
+       (map #(Integer/parseInt %))))
+
+(defn sliding-window)
 
 
 (comment
-  ;answer is 7
-  (count-depth-increases small-input)
+  (count-depth-increases test-input)
+  (count-depth-increases (read-numeric-file "src/day1/input.txt"))
   )
